23bff2857bfb1899b6e365ac74652de94668d0d1
MegaVital <kalist.fl@gmail.com>
2023-07-09 16:01:45 +0300

    newsletter

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { NewsLetter } from './NewsLetter';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+const theme = createTheme({
+  typography: {
+    button: {
+      textTransform: 'none'
+    }
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: ({ ownerState }) => ({
+          ...(ownerState.variant === 'contained' &&
+            ownerState.color === 'primary' && {
+            backgroundColor: '#232742',
+            color: '#fff',
+          }),
+          '&:hover': {
+            background: 'linear-gradient(to right, #ff537b, #ff693b)',
+          },
+        }),
+      },
+    },
+  },
+});
+
+theme.typography.h3 = {
+  fontSize: '3.1rem',
+  letterSpacing: '-0.025em',
+  lineHeight: "1",
+  '@media (max-width:1200px)': {
+    fontSize: '2.5rem',
+  },
+};
+theme.typography.h6 = {
+  fontWeight: '400',
+  fontSize: '14px',
+  letterSpacing: '0.00938em'
+};
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <React.Fragment>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <NewsLetter />
+    </ThemeProvider>
+  </React.Fragment>
 );
 
 // If you want to start measuring performance in your app, pass a function
